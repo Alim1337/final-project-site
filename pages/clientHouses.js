1b807fe20a6cda418569941cc40106f8ec7765a7
RayZeg <dzgamingmaster@gmail.com>
2023-05-09 09:58:44 +0200

    sidebar icons added

--- pages/clientHouses.js
+++ pages/clientHouses.js
@@ -14,18 +14,18 @@ function Search() {
     <div>
       <Header />
         <div className='flex bg-gray-100 text-gray-700'>
-            <div className={`${open ? "w-60" : "w-20"} h-screen relative  bg-red-400 text-gray-800`}>
+            <div className={`${open ? "w-60" : "w-20"} h-screen relative  bg-red-400`}>
                 <img 
                 src='./assests/arrow.png' 
                 className='absolute rounded-full cursor-pointer -right-3 top-9 w-7 border-2 border-dark-purple' 
                 onClick={()=>setOpen(!open)}
                 />
                 
-                <ul className={`gap-x-4 pt-6 origin-left font-medium text-xl duration-300 ${!open ? 'transform scaleX(0)' : ''}`}>
+                <ul className={`gap-x-4 pt-6 origin-left font-medium text-xl duration-300`}>
                 {menus.map((menu,index)=>(
-                    <li key={index} className='text-gray- text-s flex items-center gap-x-4 cursor-pointer p-2'>
+                    <li key={index} className={`rounded-full text-gray hover:border active:scale-95 text-s flex items-center gap-x-4 cursor-pointer p-2 ${!open ? 'transform scaleX(0)' : ''}`}>
                         <img src={`./assests/${menu.src}.png`} />
-                        <span>{menu.title}</span>
+                        <span className={`text-white transition transform ${!open ? 'transform scaleX(0)' : ''}`}>{menu.title}</span>
                     </li>
                 ))}
             </ul>
