f04e939ab345c4c76b7976d307d2eed453e2cb23
Alim <118141673+Alim1337@users.noreply.github.com>
2023-05-09 23:44:06 +0100

    bienForm commit

---
+++ components/BienForm.js
@@ -0,0 +1,142 @@
+import React from 'react';
+import Footer from './Footer';
+
+export default function BienForm({ onClose }) {
+  return (
+   
+    <div className="fixed z-10 inset-0 overflow-y-auto">
+      <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+        <div className="fixed inset-0 transition-opacity" aria-hidden="true">
+          <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
+        </div>
+
+        <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">
+          &#8203;
+        </span>
+
+        <div
+          className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full"
+          role="dialog"
+          aria-modal="true"
+          aria-labelledby="modal-headline"
+        >
+          <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+            <div className="sm:flex sm:items-start">
+              <div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-blue-100 sm:mx-0 sm:h-10 sm:w-10">
+                <svg
+                  className="h-6 w-6 text-blue-600"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+                  />
+                </svg>
+              </div>
+              <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                <h3 className="text-lg leading-6 font-medium text-gray-900" id="modal-headline">
+                  Add a new house
+                </h3>
+                <div className="mt-2">
+                  <form className="space-y-6">
+                    <div>
+                      <label htmlFor="title" className="block text-sm font-medium text-gray-700">
+                        Title
+                      </label>
+                      <div className="mt-1">
+                        <input
+                          type="text"
+                          name="title"
+                          id="title"
+                          autoComplete="title"
+                          required
+                          className="shadow-sm focus:ring-blue-500 focus:border-blue-500 block w-full sm:text-sm border-gray-300 rounded-md"
+                        />
+                      </div>
+                    </div>
+                    <div>
+                      <label htmlFor="description" className="block text-sm font-medium text-gray-700">
+                        Description
+                      </label>
+                      <div className="mt-1">
+                        <textarea
+                          id="description"
+                          name="description"
+                          rows="3"
+                          className="shadow-sm focus:ring-blue-500 focus:border-blue-500 block w-full sm:text-sm border-gray-300 rounded-md"
+                          required
+                        ></textarea>
+                                            </div>
+                    </div>
+                    <div>
+  <label className='text-black' htmlFor="price">Price range ($)</label>
+  <input
+    type="range"
+    id="price"
+    name="price"
+    min="1"
+    max="100000000000"
+    step="1"
+    defaultValue="100"
+    className="border-gray-400 border-2 rounded-lg w-full px-4 py-2 mt-2"
+  />
+</div>
+
+
+                    <div>
+                      <label  className='text-black' htmlFor="startDate">Start Date</label>
+                      <input
+                        type="date"
+                        id="startDate"
+                        name="startDate"
+                    
+                        className="border-gray-400 border-2 rounded-lg w-full px-4 py-2 mt-2"
+                      />
+                    </div>
+                    <div>
+                      <label className='text-black'  htmlFor="endDate">End Date</label>
+                      <input
+                        type="date"
+                        id="endDate"
+                        name="endDate"
+                       
+                        className="border-gray-400 border-2 rounded-lg w-full px-4 py-2 mt-2"
+                      />
+                      </div>
+               </form>
+
+                    </div>
+                  </div>
+                  <div className="flex text-black items-center justify-center mt-6">
+                    <button
+                      type="submit"
+                      className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                    >
+                      Save
+                    </button>
+                    <button
+                      type="button"
+                      onClick={() => setShowForm(false)}
+                      className="bg-gray-400 hover:bg-gray-500 text-white font-bold py-2 px-4 rounded ml-4 focus:outline-none focus:shadow-outline"
+                    >
+                      Cancel
+                    </button>
+                  </div>
+                  </div>
+                  </div>
+                </div>
+                
+    </div>
+    <Footer />
+  </div>
+        )}
+   
+
+
+
---
+++ pages/BienFormPage.js
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import Image from "next/image";
+import { useRouter } from "next/router";
+import BgLogin from "../components/bg_login";
+import Footer from "@/components/Footer";
+import Header_signup from "@/components/Header_signup";
+import BienForm from "@/components/BienForm";
+
+function Page() {
+
+ 
+  return (
+    <div>
+   <BienForm/>
+</div>
+
+
+
+  );
+}
+
+export default Page;
