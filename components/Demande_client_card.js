4a42204e5b64f6992298825920bb5fe27f5bba65
Alim <118141673+Alim1337@users.noreply.github.com>
2023-05-23 16:53:49 +0100

    done Modifier_Demande_Client

--- components/Demande_client_card.js
+++ components/Demande_client_card.js
@@ -1,25 +1,36 @@
-const Demande_client_card = ({ demandeClient }) => {
+import React from 'react';
+
+const Demande_client_card = ({ demandeClient, cardIndex }) => {
   return (
     <div>
-      <h2>Demande Client:</h2>
-      <ul>
-        {demandeClient.map((demande) => (
-          <li key={demande.id}>
+      <h2 className="text-xl font-bold mb-4">Demande Client {cardIndex}:</h2>
+      <ul className="list-disc ml-6">
+        {demandeClient.map((demande, index) => (
+          <li key={index} className="mb-4">
             {/* Display the relevant data from the demande object */}
-            <p>Type de bien: {demande.type_bien || ''}</p>
-            <p>Prix minimum: {demande.prix_minimum || ''}</p>
-            <p>Prix maximum: {demande.prix_maximum || ''}</p>
-            <p>Surface minimum: {demande.surface_minimum || ''}</p>
-            <p>Nombre de chambres minimum: {demande.nbr_chambre_minimum || ''}</p>
-            <p>Date de début de recherche: {demande.date_debut_rechercher || ''}</p>
+            <p className="mb-2">
+              <span className="font-semibold">Type de bien:</span> {demande.type_bien || ''}
+            </p>
+            <p className="mb-2">
+              <span className="font-semibold">Prix minimum:</span> {demande.prix_minimum || ''}
+            </p>
+            <p className="mb-2">
+              <span className="font-semibold">Prix maximum:</span> {demande.prix_maximum || ''}
+            </p>
+            <p className="mb-2">
+              <span className="font-semibold">Surface minimum:</span> {demande.surface_minimum || ''}
+            </p>
+            <p className="mb-2">
+              <span className="font-semibold">Nombre de chambres minimum:</span> {demande.nbr_chambre_minimum || ''}
+            </p>
+            <p className="mb-2">
+              <span className="font-semibold">Date de début de recherche:</span> {demande.date_debut_rechercher || ''}
+            </p>
           </li>
         ))}
       </ul>
     </div>
   );
 };
-
-
-
 
 export default Demande_client_card;
--- pages/Modifier_Demande_Client.js
+++ pages/Modifier_Demande_Client.js
@@ -71,18 +71,16 @@ export default function ModifierDemandeClient(props) {
         <div className="flex bg-gray-100 text-gray-700">
           <div className={`${open ? 'w-60' : 'w-20'} h-screen relative bg-red-400`}>
             <FiChevronLeft
-              className={`absolute bg-red-400 border-red-400 rounded-full h-7 cursor-pointer
-              -right-3 top-9 w-7 border-2 border-dark-purple transition transform duration-300 ease-out ${
-              open ? 'rotate-180' : ''
+              className={`absolute bg-red-400 border-red-400 rounded-full h-7 cursor-pointer -right-3 top-9 w-7 border-2 border-dark-purple transition transform duration-300 ease-out ${
+                open ? 'rotate-180' : ''
               }`}
               onClick={() => setOpen(!open)}
-              />        <ul className={`gap-x-4 space-y-3 pt-6 origin-left font-medium text-xl duration-300`}>
+            />
+            <ul className={`gap-x-4 space-y-3 pt-6 origin-left font-medium text-xl duration-300`}>
               {menus.map((menu, index) => (
                 <li
                   key={index}
-                  className={`rounded-full text-gray hover:border bg-red-500 bg-opacity-0
-                   hover:bg-opacity-70 border-opacity-70  border-red-500 active:scale-95 text-s f
-                   lex items-center gap-x-4 cursor-pointer p-2 ${
+                  className={`rounded-full text-gray hover:border bg-red-500 bg-opacity-0 hover:bg-opacity-70 border-opacity-70  border-red-500 active:scale-95 text-s flex items-center gap-x-4 cursor-pointer p-2 ${
                     !open ? 'transform scaleX(0)' : ''
                   } transition transform duration-300 ease-out`}
                 >
@@ -94,31 +92,28 @@ export default function ModifierDemandeClient(props) {
               ))}
             </ul>
           </div>
-    
-          <div className="p-7 text-2xl text-black font-semibold flex-1 h-screen">
-  {demandeClient.map((demande, index) => (
-    <div key={index}>
-      <h2>Type de bien: {demande.type_bien}</h2>
-      <p>Prix minimum: {demande.prix_minimum}</p>
-      <p>Prix maximum: {demande.prix_maximum}</p>
-      <p>Surface minimum: {demande.surface_minimum}</p>
-      <p>Nombre de chambres minimum: {demande.nbr_chambre_minimum}</p>
-      <p>Date de début de recherche: {demande.date_debut_rechercher}</p>
-    </div>
-  ))}
-</div>
+          <div className="p-7 text-2xl text-black font-semibold flex-1 h-screen overflow-auto">
+            <h2 className="text-xl font-bold mb-4">Tu As  {demandeClient.length ? demandeClient.length : ''} Demande Client:</h2>
+            <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+            {demandeClient.map((demande, index) => (
+  <div key={index} className="mb-4">
+    <Demande_client_card demandeClient={[demande]} cardIndex={index + 1} className="hover:scale-105 transition-all duration-300" />
+  </div>
+))}
 
+            </div>
+          </div>
         </div>
-        <div className='p-20 py-0'>
-          <h2 className='font-mono text-green-600'>Client Connected Name:</h2>
-          <h2 className='font-mono text-green-600'>{ClientName}</h2>
+        <div className="p-20 py-0 bg-gray-50">
+          <h2 className="font-mono text-green-600">Client Connected Name:</h2>
+          <h2 className="font-mono text-green-600">{ClientName}</h2>
         </div>
-        <div className='p-0'>
-          <h2 className='font-mono text-green-600'>Client Connected Email: </h2>
-          <h2 className='font-mono text-green-600'>{ClientEmail}</h2>
+        <div className="p-0 bg-gray-50">
+          <h2 className="font-mono text-green-600">Client Connected Email: </h2>
+          <h2 className="font-mono text-green-600">{ClientEmail}</h2>
         </div>
       </main>
       <Footer />
     </div>
-);
-}    
+  );
+}
