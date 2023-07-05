458bdcea9e7cdf56c8c8d27d88652b0a6aa499e4
Alim <118141673+Alim1337@users.noreply.github.com>
2023-07-05 04:11:34 +0100

    +

--- components/CardHouse.js
+++ components/CardHouse.js
@@ -104,18 +104,7 @@ function CardHouse({ id_biens, description, type_bien, adresse, ville, code_post
       <p className="text-xl  mb-2">Postal Code: {code_postal && code_postal.join(', ')}</p>
       <p className="text-xl mb-2">Estimated Price: {prix_estime}</p>
       <p className="text-xl mb-2">State: {etat}</p>
-      {token && (
-      <form onSubmit={handleSubmitIMAGE}>
-        <div>
-          <label className="text-black text-xl" htmlFor="image">Image:</label>
-       
-            <div>
-              <input type="file" id="image" accept="image/*" onChange={handleImageChange} />
-            </div>
-          
-        </div>
-        <button type="submit">Submit</button>
-      </form> )}
+     
       {token && (
         <button
           className="text-green-600 bg-white px-8 py-4 font-mono shadow-md rounded-full font-bold my-3 hover:shadow-2xl active:scale-90 transition duration-150"
