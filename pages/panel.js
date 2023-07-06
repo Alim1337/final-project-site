d17d2ef2e892b0eb2663fabaa5d7600960cc6ae6
RayZeg <61918197+RayZeg@users.noreply.github.com>
2023-07-06 13:47:07 +0200

    panel changes

--- pages/panel.js
+++ pages/panel.js
@@ -10,7 +10,7 @@ import DemandeClientCard from '@/components/DemandeClientCard';
 import { HiOutlineHome } from "react-icons/hi2";
 import { HiUser } from "react-icons/hi2";
 import { FaChalkboardTeacher } from "react-icons/fa";
-import { IoIosHand } from "react-icons/io";
+import { HiOutlineCog } from "react-icons/hi2";
 import jwt from 'jsonwebtoken';
 
 /** @param {import('next').InferGetStaticPropsType<typeof getStaticProps> } props */
@@ -22,16 +22,15 @@ export default function ProprietaireHouses({ exploreData, cardsData }) {
   const [showConfirmationWindow, setShowConfirmationWindow] = useState(false); // New state variable
 
   const menus = [
-    { title: 'Gestion de profil', icon: HiUser },
-    { title: 'Gestion des annonces', icon: FaChalkboardTeacher },
-    { title: 'Gestion des biens', icon: HiOutlineHome },
+    { title: 'Dashboard', icon: HiOutlineHome },
+    
     { title: 'Support', icon: FiPlus },
     {
       title: 'Devenir VIP',
       icon: FiChevronDown,
       button: true,
     },
-    { title: 'settings', icon: IoIosHand },
+    { title: 'Paramètre', icon: HiOutlineCog },
   ];
 
   const router = useRouter();
@@ -156,41 +155,40 @@ export default function ProprietaireHouses({ exploreData, cardsData }) {
                 onClick={() => setOpen(!open)}
               />
              <ul className={`gap-x-4 space-y-3 pt-6 origin-left font-medium text-xl duration-300`}>
-  {menus.map((menu, index) => (
-    <li
-      key={index}
-      className={`rounded-full text-gray hover:border bg-red-500 bg-opacity-0 hover:bg-opacity-70 
-      border-opacity-70  border-red-500 active:scale-95 text-s flex items-center gap-x-4 cursor-pointer p-2 ${
-        !open ? 'transform scaleX(0)' : ''
-      } transition transform duration-300 ease-out`}
-    >
-      {menu.button ? (
-        <button className="flex items-center gap-x-2" onClick={handleDevenirVIP}>
-          {React.createElement(menu.icon, { className: 'text-white' })}
-          <span className={`text-white transition transform ${!open ? 'transform scaleX(0)' : ''}`}>
-            {menu.title}
-          </span>
-        </button>
-      ) : (
-        <>
-          {React.createElement(menu.icon, { className: 'text-white' })}
-          <span className={`text-white transition transform ${!open ? 'transform scaleX(0)' : ''}`}>
-            {menu.title}
-          </span>
-        </>
-      )}
-      {menu.button1 && (
-        <button className="flex items-center gap-x-2" onClick={handleModifierProfil}>
-          {React.createElement(menu.icon, { className: 'text-white' })}
-          <span className={`text-white transition transform ${!open ? 'transform scaleX(0)' : ''}`}>
-            {menu.title}
-          </span>
-        </button>
-      )}
-    </li>
-  ))}
-</ul>
-
+              {menus.map((menu, index) => (
+              <li
+                key={index}
+                className={`rounded-full text-gray hover:border bg-red-500 bg-opacity-0 hover:bg-opacity-70 
+                border-opacity-70  border-red-500 active:scale-95 text-s flex items-center gap-x-4 cursor-pointer p-2 ${
+                  !open ? 'transform scaleX(0)' : ''
+                } transition transform duration-300 ease-out`}
+              >
+              {menu.button ? (
+                <button className="flex items-center gap-x-2" onClick={handleDevenirVIP}>
+                  {React.createElement(menu.icon, { className: 'text-white' })}
+                  <span className={`text-white transition transform ${!open ? 'transform scaleX(0)' : ''}`}>
+                    {menu.title}
+                  </span>
+                </button>
+                ) : (
+                  <>
+                    {React.createElement(menu.icon, { className: 'text-white' })}
+                    <span className={`text-white transition transform ${!open ? 'transform scaleX(0)' : ''}`}>
+                      {menu.title}
+                    </span>
+                  </>
+              )}
+              {menu.button1 && (
+                <button className="flex items-center gap-x-2" onClick={handleModifierProfil}>
+                  {React.createElement(menu.icon, { className: 'text-white' })}
+                  <span className={`text-white transition transform ${!open ? 'transform scaleX(0)' : ''}`}>
+                    {menu.title}
+                  </span>
+                </button>
+              )}
+              </li>
+            ))}
+            </ul>
             </div>
             <div className="p-7 text-2xl font-semibold flex-1 h-screen">
               <h1 className="font-bold text-gray-700 text-4xl">Gestion Des Biens</h1>
@@ -206,22 +204,15 @@ export default function ProprietaireHouses({ exploreData, cardsData }) {
                 </div>                
               )}
                {userType=== 'proprietaire' && (
-                
-                <button className="text-left -space-x-px sm:grid-cols-2 lg:grid-cols-3 
-                xl:grid-cols-4 text-gray-690 transition duration-300 ease-in-out
-                 transform hover:scale-105 hover:cursor-pointer font-mono bg-transparent" 
-                 onClick={() => router.push('/BienFormProprietaire')}>
-                <AjoutCard key="gestion" text="Ajouter un bien
-" />
 
-              
+                <button className="text-left -space-x-px sm:grid-cols-2 lg:grid-cols-3 
+                  xl:grid-cols-4 text-gray-690 transition duration-300 ease-in-out
+                  transform hover:scale-105 hover:cursor-pointer font-mono bg-transparent" 
+                  onClick={() => router.push('/BienFormProprietaire')}>
+                  <AjoutCard key="gestion" text="Ajouter un bien" />
                 </button>
-                
-                
               )}
-                
-                
-                
+
               {userType === 'proprietaire' && (
                 <button className="text-left  sm:grid-cols-2 lg:grid-cols-3 
                 xl:grid-cols-4 text-gray-690 transition duration-300 ease-in-out
@@ -229,6 +220,7 @@ export default function ProprietaireHouses({ exploreData, cardsData }) {
                   <GestionCard key="gestion" text="Modifier un bien" />
                 </button>
               )}
+
               {userType === 'proprietaire' && (
               <button className="text-left sm:grid-cols-2 lg:grid-cols-3 
               xl:grid-cols-4 text-gray-690 transition duration-300 ease-in-out
@@ -246,14 +238,18 @@ export default function ProprietaireHouses({ exploreData, cardsData }) {
                 >
                   <AjoutCard key="gestion" text="Negotiations sur votre biens" />
                 </button>       
-                    )}
-         
-             
+                )}
+              <button 
+                className="text-left sm:grid-cols-2 lg:grid-cols-3 
+                xl:grid-cols-4 text-gray-690 transition duration-300 ease-in-out
+                transform hover:scale-105 hover:cursor-pointer font-mono bg-transparent" 
+                onClick={() => router.push('/homesList')}>
+                <AjoutCard key="gestion" text="Consulter les biens"/>
+              </button>
             </div>
+
             <div className="p-7 text-2xl font-semibold flex-1 h-screen">
-            
               <h1 className="font-bold text-gray-700 text-4xl">Gestion Des Annonces</h1>
-              
                 <button
                   className="text-left sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4
                    text-gray-690 transition duration-300 ease-in-out transform hover:scale-105 
@@ -272,8 +268,6 @@ export default function ProprietaireHouses({ exploreData, cardsData }) {
                   <AjoutCard key="gestion" text="Consulter Et Modifier Votre Demandes Personnalisée" />
                 </button>
               
-          
-           
                 <button
                   className="text-left sm:grid-cols-2 lg:grid-cols-3 
                   xl:grid-cols-4 text-gray-690 transition duration-300 ease-in-out
@@ -283,114 +277,111 @@ export default function ProprietaireHouses({ exploreData, cardsData }) {
                   <AjoutCard key="gestion" text="Negotiations avec les proprietaires" />
                 </button>
             </div>
-            
           </div>
         </main>
       </div>
+
       <div>
        {/* VIP Window */}
        {showVIPWindow && (
-  <div className="fixed inset-0 flex items-center justify-center z-10 bg-gray-800 bg-opacity-75">
-    <div className="flex flex-col items-center p-4 bg-white rounded-lg shadow-lg">
-      <div className="p-3 mr-4 text-yellow-500 bg-yellow-100 rounded-full">
-        <svg
-          className="w-5 h-5"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        >
-          <circle cx="12" cy="8" r="7"></circle>
-          <polyline points="8.21 13.89 7 23 12 20 17 23 15.79 13.88"></polyline>
-        </svg>
-      </div>
-      <div className="text-left">
-        <p className="mb-2 text-sm font-medium text-gray-600">DevienirVIP</p>
-      </div>
-      <div className="flex justify-between mt-4">
-      <button
-  className="text-white bg-gradient-to-r from-green-400 via-green-500
-  to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none
-  focus:ring-green-300 dark:focus:ring-green-800 shadow-lg
-  shadow-green-500/50 dark:shadow-lg dark:shadow-green-800/80
-  font-medium rounded-lg text-sm px-5 py-2.5 text-left mr-2 mb-2"
-  onClick={handleJaipaye} // Call the handleJaipaye function on button click
->
-  Jai payé
-</button>
+        <div className="fixed inset-0 flex items-center justify-center z-10 bg-gray-800 bg-opacity-75">
+          <div className="flex flex-col items-center p-4 bg-white rounded-lg shadow-lg">
+            <div className="p-3 mr-4 text-yellow-500 bg-yellow-100 rounded-full">
+              <svg
+                className="w-5 h-5"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <circle cx="12" cy="8" r="7"></circle>
+                <polyline points="8.21 13.89 7 23 12 20 17 23 15.79 13.88"></polyline>
+              </svg>
+            </div>
+            <div className="text-left">
+              <p className="mb-2 text-sm font-medium text-gray-600">DevienirVIP</p>
+            </div>
+            <div className="flex justify-between mt-4">
+              <button
+                className="text-white bg-gradient-to-r from-green-400 via-green-500
+                to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none
+                focus:ring-green-300 dark:focus:ring-green-800 shadow-lg
+                shadow-green-500/50 dark:shadow-lg dark:shadow-green-800/80
+                font-medium rounded-lg text-sm px-5 py-2.5 text-left mr-2 mb-2"
+                onClick={handleJaipaye} // Call the handleJaipaye function on button click
+              >
+                Jai payé
+              </button>
 
-        <button
-          className="text-white bg-gradient-to-r from-red-400 via-red-500
-           to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none
-            focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg
-             dark:shadow-red-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-left mr-2 mb-2"
-          onClick={() => {
-            setShowVIPWindow(false);
-          }}
-        >
-          Jai pas payé encore
-        </button>
-      </div>
-    </div>
-  </div>
-)}
- {/* Confirmation Window */}
- {showConfirmationWindow && (
-  <div className="fixed inset-0 flex items-center justify-center z-10 bg-gray-800 bg-opacity-75">
-    <div className="flex flex-col items-center p-4 bg-white rounded-lg shadow-lg">
-      <div className="p-3 mr-4 text-yellow-500 bg-yellow-100 rounded-full">
-        {/* Existing code */}
-        <svg
-          className="w-5 h-5"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        >
-          <circle cx="12" cy="8" r="7"></circle>
-          <polyline points="8.21 13.89 7 23 12 20 17 23 15.79 13.88"></polyline>
-        </svg>
-      </div>
-      <div className="text-left">
-        {/* Existing code */}
-      </div>
-      <h1 className="text-black font-semi-bold text-xl mt-4">On considère que le paiement est fait</h1>
+              <button
+                className="text-white bg-gradient-to-r from-red-400 via-red-500
+                to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none
+                  focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg
+                  dark:shadow-red-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-left mr-2 mb-2"
+                onClick={() => {
+                  setShowVIPWindow(false);
+                }}
+              >
+                Jai pas payé encore
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+      {/* Confirmation Window */}
+      {showConfirmationWindow && (
+        <div className="fixed inset-0 flex items-center justify-center z-10 bg-gray-800 bg-opacity-75">
+          <div className="flex flex-col items-center p-4 bg-white rounded-lg shadow-lg">
+            <div className="p-3 mr-4 text-yellow-500 bg-yellow-100 rounded-full">
+              {/* Existing code */}
+              <svg
+                className="w-5 h-5"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <circle cx="12" cy="8" r="7"></circle>
+                <polyline points="8.21 13.89 7 23 12 20 17 23 15.79 13.88"></polyline>
+              </svg>
+            </div>
+            <div className="text-left">
+              {/* Existing code */}
+            </div>
+            <h1 className="text-black font-semi-bold text-xl mt-4">On considère que le paiement est fait</h1>
 
-      <div className="flex justify-between mt-4">
-        
-        <button
-          className="text-white bg-gradient-to-r from-green-400 via-green-500
-          to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none
-          focus:ring-green-300 dark:focus:ring-green-800 shadow-lg
-          shadow-green-500/50 dark:shadow-lg dark:shadow-green-800/80
-          font-medium rounded-lg text-sm px-5 py-2.5 text-left mr-2 mb-2"
-          onClick={handleConfirmation} // Call the handleConfirmation function on button click
-        >
-          Oui je suis sûr
-        </button>
-        <button
-          className="text-white bg-gradient-to-r from-red-400 via-red-500
-          to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none
-          focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg
-          dark:shadow-red-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-left mr-2 mb-2"
-          onClick={() => setShowConfirmationWindow(false)} // Close the confirmation window
-        >
-          Non je ne suis pas sûr
-        </button>
-        
-      </div>
-    </div>
-  </div>
-)}
-
+            <div className="flex justify-between mt-4">
+              
+              <button
+                className="text-white bg-gradient-to-r from-green-400 via-green-500
+                to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none
+                focus:ring-green-300 dark:focus:ring-green-800 shadow-lg
+                shadow-green-500/50 dark:shadow-lg dark:shadow-green-800/80
+                font-medium rounded-lg text-sm px-5 py-2.5 text-left mr-2 mb-2"
+                onClick={handleConfirmation} // Call the handleConfirmation function on button click
+              >
+                Oui je suis sûr
+              </button>
+              <button
+                className="text-white bg-gradient-to-r from-red-400 via-red-500
+                to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none
+                focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg
+                dark:shadow-red-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-left mr-2 mb-2"
+                onClick={() => setShowConfirmationWindow(false)} // Close the confirmation window
+              >
+                Non je ne suis pas sûr
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
        </div>
-
       <Footer />
     </div>
   );
