981671324bf2064f4e7e1555afd34c34759ad358
Alim <118141673+Alim1337@users.noreply.github.com>
2023-07-06 13:10:51 +0100

    hide i like this

--- components/CardHouse.js
+++ components/CardHouse.js
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
-import BgLogin from "../components/bg_login";
-import { useState } from 'react';
-
-import { toast, ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
 import Image from "next/image";
-function CardHouse({ id_biens, description, type_bien, adresse, ville, code_postal, prix_estime, etat,nbrChambre, Proprietaire, token, onInterestedClick }) {
+import jwt from 'jsonwebtoken';
+
+function CardHouse({ id_biens, description, type_bien, adresse, ville, code_postal, prix_estime, etat, nbrChambre, Proprietaire, token, onInterestedClick }) {
   const [image, setImage] = useState(null);
+  const [decodedToken, setDecodedToken] = useState(null);
 
   const router = useRouter();
+
+  useEffect(() => {
+    if (token) {
+      setDecodedToken(jwt.decode(token));
+    }
+  }, [token]);
+
   const handleImageChange = (event) => {
     const file = event.target.files[0];
     const reader = new FileReader();
@@ -40,8 +45,6 @@ function CardHouse({ id_biens, description, type_bien, adresse, ville, code_post
     // Save the image in localStorage
     localStorage.setItem(`image_${id_biens}`, JSON.stringify(image));
   };
-
-  
 
   const getStoredImage = () => {
     const storedImage = localStorage.getItem(`image_${id_biens}`);
@@ -88,7 +91,7 @@ function CardHouse({ id_biens, description, type_bien, adresse, ville, code_post
   return (
     <div className="bg-white rounded-lg shadow-md p-6 transition duration-300 ease-out transform hover:scale-105 hover:shadow-lg">
       <div className="relative h-32 w-full mb-4">
-      <Image
+        <Image
           src={getStoredImage() || getImageSrc()}
           alt="Property Image"
           layout="fill"
@@ -108,13 +111,17 @@ function CardHouse({ id_biens, description, type_bien, adresse, ville, code_post
       <p className="text-xl mb-2">Nombre de chambres: {nbrChambre}</p>
 
      
-      {token && (
-        <button
-          className="text-green-600 bg-white px-8 py-4 font-mono shadow-md rounded-full font-bold my-3 hover:shadow-2xl active:scale-90 transition duration-150"
-          onClick={() => onInterestedClick(id_biens, Proprietaire.id_proprietaire)}
-        >
-          I like this
-        </button>
+      {decodedToken && decodedToken.id === Proprietaire.id_proprietaire ? (
+        <p className="text-red-500 text-xl font-bold mt-4">Votre bien </p>
+      ) : (
+        token && (
+          <button
+            className="text-green-600 bg-white px-8 py-4 font-mono shadow-md rounded-full font-bold my-3 hover:shadow-2xl active:scale-90 transition duration-150"
+            onClick={() => onInterestedClick(id_biens, Proprietaire.id_proprietaire)}
+          >
+            I like this
+          </button>
+        )
       )}
     </div>
   );
