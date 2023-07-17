9af590c44c1e156e38b99136102556a64e7081f8
Alim <118141673+Alim1337@users.noreply.github.com>
2023-07-17 13:18:32 +0100

    Revert "routing"
    This reverts commit 35b6509dd4720422d34bf28b45ef5bfc6a182ec6.

--- pages/Chat_client.js
+++ pages/Chat_client.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
-import jwt from 'jsonwebtoken';
 
 const ChatClient = () => {
     const [messages, setMessages] = useState([]);
